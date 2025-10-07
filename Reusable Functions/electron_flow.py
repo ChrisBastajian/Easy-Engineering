7ca89dea31ae239a8db5d2c73f66ca88fac36b24
ChrisBastajian <chrisbastajian@gmail.com>
2025-10-07 08:09:38 -0400

    turned into function

--- Reusable Functions/electron_flow.py
+++ Reusable Functions/electron_flow.py
@@ -7,18 +7,18 @@ class ElectronFlow(Scene):
         self.add(conductor)
 
         # Call reusable function
-        self.animate_electron_flow(conductor,
+        electron_flow = self.animate_electron_flow(conductor,
                                    num_electrons=12,
                                    drift_speed=0.02,
                                    signal_speed=0.2)
 
-    def animate_electron_flow(self, conductor: Line,
+        self.play(electron_flow) #play the animation
+
+    def animate_electron_flow(self, conductor,
                               num_electrons=10,
                               drift_speed=0.2,
                               signal_speed=2.0):
-        #Animates electrons moving through a conductor
-
-        # Place electrons evenly spaced along the conductor
+        # Create electrons
         electrons = VGroup(*[
             Dot(color=BLUE, radius=0.08).move_to(
                 conductor.point_from_proportion(i / (num_electrons - 1))
@@ -27,17 +27,12 @@ class ElectronFlow(Scene):
         ])
         self.add(electrons)
 
-        # Drift motion of electrons
-        electron_anims = []
-        for e in electrons:
-            # Move right across the conductor very slowly
-            target = e.copy().shift(RIGHT * 4)
-            electron_anims.append(MoveAlongPath(e, conductor, rate_func=linear))
+        # Define drift animation along the conductor
+        electron_anims = [
+            MoveAlongPath(e, conductor, rate_func=linear)
+            for e in electrons
+        ]
 
-        # Play both animations simultaneously
-        self.play(
-            AnimationGroup(*electron_anims, run_time=1 / drift_speed, lag_ratio=0.3),
-            run_time=1 / signal_speed
-        )
-        self.wait()
+        # Return a combined animation instead of playing it here
+        return AnimationGroup(*electron_anims, run_time=1 / drift_speed, lag_ratio=0.3)
 
